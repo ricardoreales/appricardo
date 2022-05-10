430bfb07b60ab014f26a0abb483464a150b02a7e
Ricardo Reales <ricardorealesabrams@gmail.com>
2022-05-09 23:12:22 -0300

    creacion del nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar() {
+    return<>
+        <img src="#"></img>
+        <button>inicio</button>
+        <h1>autos</h1>
+        <h1>motos</h1>
+        <h1>bicicletas</h1>
+        <button>inicio</button>
+
+
+
+    </>
+}
+
+export default NavBar
